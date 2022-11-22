f3af65acff8431fc8eb09141ffd0816a06587e9c
Logen <47022821+Logenleedev@users.noreply.github.com>
2022-11-22 09:14:00 -0600

    add double linked list and pop_front method

--- LinkedList/double_linked_list.py
+++ LinkedList/double_linked_list.py
@@ -25,6 +25,43 @@ class DoublyLinkedList:
             self.head.prev = None
             self.head.next = None
 
+    def push_back(self, new_data): # Adding an element after the last element
+        new_node = Node(new_data)
+        new_node.prev = self.tail
+
+        if self.tail == None: # checks whether the list is empty, if so make both head and tail as new node
+            self.head = new_node 
+            self.tail = new_node
+            new_node.next = None # the first element's previous pointer has to refer to null
+                
+        else: # If list is not empty, change pointers accordingly
+            self.tail.next = new_node
+            new_node.next = None
+            self.tail = new_node 
+
+    def peek_front(self): # returns first element
+        if self.head == None and self.tail == None: 
+            print("Nothing is here")
+        else:
+            return self.head.data
+    
+    def peek_back(self): # returns last element
+        if self.head == None and self.tail == None: 
+            print("Nothing is here")
+        else:
+            return self.tail.data
+
+    def pop_front(self): # removes and returns the first element
+        if self.head == None: 
+            print("Cannot pop since the list is empty")
+            return None
+        else:
+            self.head.next.prev = None
+            self.head = self.head.next
+            
+            return self.head.data
+    
+
     def printLinkedList(self):
         if (self.head == None):
             print("Nothing here")
@@ -36,6 +73,9 @@ class DoublyLinkedList:
 
 if __name__ == "__main__":
     L = DoublyLinkedList()
+    L.push_front(6)
     L.push_front(5)
     L.push_front(3)
+    L.pop_front()
+    L.pop_front()
     L.printLinkedList()
