c204b5f36eb9fe1fd6a1df510f07348bb11c60f9
Logen <47022821+Logenleedev@users.noreply.github.com>
2022-11-29 16:16:39 -0600

    push leetcode 206

--- LinkedList/LeetCode_Solution/Leetcode_206_Reverse_Linked_List.py
+++ LinkedList/LeetCode_Solution/Leetcode_206_Reverse_Linked_List.py
@@ -14,7 +14,7 @@ class Solution:
 # Method 2 - Recursion 
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        if head.next == None or head.next == None:
+        if head == None or head.next == None:
             return head
         p = self.reverseList(head.next)
         head.next.next = head
