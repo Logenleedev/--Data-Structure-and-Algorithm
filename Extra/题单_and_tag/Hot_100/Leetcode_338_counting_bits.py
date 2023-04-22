ea5ab5c56ed86758117aa868a52facd5e9255b6b
Logen <47022821+Logenleedev@users.noreply.github.com>
2023-04-21 20:08:30 -0500

    push update

--- Extra/题单_and_tag/Hot_100/Leetcode_338_counting_bits.py
+++ Extra/题单_and_tag/Hot_100/Leetcode_338_counting_bits.py
@@ -1,3 +1,4 @@
+# Method 1:
 class Solution:
     def countBits(self, n: int) -> List[int]:
         ans = []
@@ -6,3 +7,20 @@ class Solution:
             ans.append(bin(i)[2:].count("1"))
 
         return ans 
+
+# Method 2:
+class Solution:
+    def countBits(self, n: int) -> List[int]:
+        
+
+        ans = [0] * (n + 1)
+        for i in range(1, n + 1):
+            ans[i] = ans[i & (i - 1)] + 1
+        
+        return ans 
+
+# 1 -> 1
+# 10 -> 2
+# 11 -> 3 
+# 100 -> 4 
+# 101 -> 5 
